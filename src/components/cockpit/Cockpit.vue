3add4923af2f138c0af71c508fa5d30e23f75dcd
vkasraj <vikasraj1911@gmail.com>
2018-01-29 15:19:04 +0530

    async import of editors for  different devices

--- src/components/cockpit/Cockpit.vue
+++ src/components/cockpit/Cockpit.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="workspace d-flex flex-wrap flex-grow">
         <div class="col-12 col-md-6 px-0 pl-lg-0 pr-lg-1">
-            <!-- <app-editors></app-editors> -->
-            <app-editors-mobile></app-editors-mobile>
+            <template v-if="isMobile">
+                <app-editors-mobile></app-editors-mobile>
+            </template>
+            <template v-else>
+                <app-editors></app-editors>
+            </template>
         </div>
         <div class="col-12 col-md-6 px-0 pr-lg-0 pl-lg-1">
             <app-render></app-render>
@@ -11,11 +15,18 @@
 </template>
 
 <script>
-import EditorsMobile from "../editors/mobile/EditorsMobile.vue";
-import Editors from "../editors/Editors.vue";
+const EditorsMobile = () => import("../editors/mobile/EditorsMobile.vue");
+const Editors = () => import("../editors/Editors.vue");
+// import EditorsMobile from "../editors/mobile/EditorsMobile.vue";
+// import Editors from "../editors/Editors.vue";
 import Render from "../render/Render.vue";
 
 export default {
+    data() {
+        return {
+            isMobile: /Mobi/.test(navigator.userAgent)
+        }
+    },
     components: {
         appEditorsMobile: EditorsMobile,
         appEditors: Editors,
