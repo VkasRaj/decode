2eec5687608ec380bd3b8af64b46fe203aca77cf
vkasraj <vikasraj1911@gmail.com>
2018-01-30 16:41:17 +0530

    iframe: sandbox mode

--- src/components/render/Render.vue
+++ src/components/render/Render.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="render" class="h-100 bg-white">
-    <iframe src="" frameborder="0" class="bg-white w-100 h-100" :srcdoc="output"></iframe>
+    <iframe 
+      :srcdoc="output"
+      class="bg-white" 
+      frameborder="0"
+      sandbox="allow-scripts"
+      width="100%"
+      height="100%"></iframe>
   </div>
 </template>
 
