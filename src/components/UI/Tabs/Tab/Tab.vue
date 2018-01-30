73451243f2876ff8101b2d7385a5d7b6962b5ccd
vkasraj <vikasraj1911@gmail.com>
2018-01-30 14:24:01 +0530

    tab components

---
+++ src/components/UI/Tabs/Tab/Tab.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-show="isActive" 
+        :id="title" 
+        role="tabpanel" 
+        aria-labelledby="home-tab">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        selected: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            isActive: false
+        }
+    },
+    created() {
+        this.isActive = this.selected;
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/UI/Tabs/Tabs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tabs-wrapper d-flex flex-column h-100">
+    <ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
+      <li class="nav-item" v-for="(tab, i) in tabs" :key="i">
+        <a 
+          class="nav-link rounded-0 py-0 text-uppercase"
+          :class="{'active': tab.isActive, 'text-white': !tab.isActive}"
+          @click="selectTab(tab)"
+          id="home-tab" 
+          data-toggle="tab" 
+          role="tab" 
+          aria-controls="home" 
+          aria-selected="true">{{ tab.title }}</a>
+      </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabs: null,
+    }
+  },
+  methods: {
+    selectTab(tab) {
+      this.tabs.forEach(element => {
+        element.isActive = (tab.title == element.title);
+      });
+    }
+  },
+  created() {
+    this.tabs = this.$children
+  }
+};
+</script>
+
+<style>
+
+</style>
