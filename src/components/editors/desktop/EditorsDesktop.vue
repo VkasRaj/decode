8c8a51d10b0ad529a97ff16b5a35f5e5cb2470c9
vkasraj <vikasraj1911@gmail.com>
2018-02-09 18:35:34 +0530

    header run button styles & little change

--- src/components/editors/desktop/EditorsDesktop.vue
+++ src/components/editors/desktop/EditorsDesktop.vue
@@ -1,62 +1,64 @@
 <template>
     <div id="editor-wrapper" class="h-100">
-        <app-modal v-if="isModal === true" @close="isModal = false">
-            <app-modal-body>
-                <h5 class="mb-3 letter-spacing-1 text-uppercase">Code Setting</h5>
-                <app-tabs :light='true'>
-                    <app-tab title='html' :selected="whichTab === 'html'">
-                        <app-form-group label="Add Class(es) to <html>">
-                            <small slot="extra-info-top" class="d-block text-secondary mb-2">Multiple classes should be separated by space.</small>
-                            <app-input 
-                                slot="input" 
-                                placeholder="e.g. bg-primary text-white"
-                                :val="htmlConfig.htmlTagClasses"
-                                @onInput="htmlTagClasses = $event"></app-input>
-                        </app-form-group>
-                        <app-form-group label="Stuff for <head>">
-                            <small slot="extra-info-top" class="d-block text-secondary mb-2">All the HTML tags that will go into &lt;head&gt;</small>
-                            <app-input type='textarea' 
-                                slot="input" 
-                                placeholder="e.g. <meta> <link> <script>" 
-                                :val="htmlConfig.headStuff"
-                                @onInput="headStuff = $event"></app-input>
-                        </app-form-group>
-                        <div class="text-right">
-                            <app-button type='light' @clicked="isModal = false">Close</app-button>
-                            <app-button type='dark' @clicked="setHtmlConfig({htmlTagClasses, headStuff})">Save</app-button>
-                        </div>
-                    </app-tab>
-                    <app-tab title='css' :selected="whichTab === 'css'">
-                        <app-form-group label="Add External Stylesheets/Library (URLs)">
-                            <small slot="extra-info-top" class="d-block text-secondary mb-2">Any Url's added here will be added as &lt;link&gt;s in order, and before the CSS in the editor.</small>
-                            <app-input 
-                                slot="input" 
-                                placeholder="e.g. https://getbootstrap.com"
-                                :val="cssConfig.stylesheets"
-                                @onInput="stylesheets = $event"></app-input>
-                        </app-form-group>
-                        <div class="text-right">
-                            <app-button type='light' @clicked="isModal = false">Close</app-button>
-                            <app-button type='dark' @clicked="setCssConfig({ stylesheets })">Save</app-button>
-                        </div>
-                    </app-tab>
-                    <app-tab title='js' :selected="whichTab === 'js'">
-                        <app-form-group label="Add External Scripts/Library (URLs)">
-                            <small slot="extra-info-top" class="d-block text-secondary mb-2">Any Url's added here will be added as &lt;script&gt;s in order, and before the Javascript in the editor.</small>
-                            <app-input 
-                                slot="input" 
-                                placeholder="e.g. https://code.jquery.com"
-                                :val="jsConfig.scripts"
-                                @onInput="scripts = $event"></app-input>
-                        </app-form-group>
-                        <div class="text-right">
-                            <app-button type='light' @clicked="isModal = false">Close</app-button>
-                            <app-button type='dark' @clicked="setJsConfig({ scripts })">Save</app-button>
-                        </div>
-                    </app-tab>
-                </app-tabs>
-            </app-modal-body>
-        </app-modal>
+        <template v-if="isModal === true">
+            <app-modal @close="isModal = false">
+                <app-modal-body>
+                    <h5 class="mb-3 letter-spacing-1 text-uppercase">Code Settings</h5>
+                    <app-tabs :light='true'>
+                        <app-tab title='html' :selected="whichTab === 'html'">
+                            <app-form-group label="Add Class(es) to <html>">
+                                <small slot="extra-info-top" class="d-block text-secondary mb-2">Multiple classes should be separated by space.</small>
+                                <app-input 
+                                    slot="input" 
+                                    placeholder="e.g. bg-primary text-white"
+                                    :val="htmlConfig.htmlTagClasses"
+                                    @onInput="htmlTagClasses = $event"></app-input>
+                            </app-form-group>
+                            <app-form-group label="Stuff for <head>">
+                                <small slot="extra-info-top" class="d-block text-secondary mb-2">All the HTML tags that will go into &lt;head&gt;</small>
+                                <app-input type='textarea' 
+                                    slot="input" 
+                                    placeholder="e.g. <meta> <link> <script>" 
+                                    :val="htmlConfig.headStuff"
+                                    @onInput="headStuff = $event"></app-input>
+                            </app-form-group>
+                            <div class="text-right">
+                                <app-button type='light' @clicked="isModal = false">Close</app-button>
+                                <app-button type='dark' @clicked="setHtmlConfig({htmlTagClasses, headStuff})">Save</app-button>
+                            </div>
+                        </app-tab>
+                        <app-tab title='css' :selected="whichTab === 'css'">
+                            <app-form-group label="Add External Stylesheets/Library (URLs)">
+                                <small slot="extra-info-top" class="d-block text-secondary mb-2">Any Url's added here will be added as &lt;link&gt;s in order, and before the CSS in the editor.</small>
+                                <app-input 
+                                    slot="input" 
+                                    placeholder="e.g. https://getbootstrap.com"
+                                    :val="cssConfig.stylesheets"
+                                    @onInput="stylesheets = $event"></app-input>
+                            </app-form-group>
+                            <div class="text-right">
+                                <app-button type='light' @clicked="isModal = false">Close</app-button>
+                                <app-button type='dark' @clicked="setCssConfig({ stylesheets })">Save</app-button>
+                            </div>
+                        </app-tab>
+                        <app-tab title='js' :selected="whichTab === 'js'">
+                            <app-form-group label="Add External Scripts/Library (URLs)">
+                                <small slot="extra-info-top" class="d-block text-secondary mb-2">Any Url's added here will be added as &lt;script&gt;s in order, and before the Javascript in the editor.</small>
+                                <app-input 
+                                    slot="input" 
+                                    placeholder="e.g. https://code.jquery.com"
+                                    :val="jsConfig.scripts"
+                                    @onInput="scripts = $event"></app-input>
+                            </app-form-group>
+                            <div class="text-right">
+                                <app-button type='light' @clicked="isModal = false">Close</app-button>
+                                <app-button type='dark' @clicked="setJsConfig({ scripts })">Save</app-button>
+                            </div>
+                        </app-tab>
+                    </app-tabs>
+                </app-modal-body>
+            </app-modal>
+        </template>
 
         <app-editor 
             :name="'html'" 
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -7,14 +7,13 @@
       <div class="navbar-collapse" id="decoderNavbar">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <small>
-              <a class="pointer nav-link text-uppercase text-white letter-spacing-1" 
-                @click="showOutput">
-                <svg v-if="saved" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="fill: #fff;"  width=".6rem"><path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"/></svg>
-                <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" style="fill: #fff;" width=".6rem"><title>to be done</title><circle cx="50" cy="50" r="50"/></svg>
+            <a class="nav-link">
+              <button class="btn btn-light btn-sm pointer text-uppercase letter-spacing-1 badge-pill" @click="showOutput">
+                <svg v-if="saved" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="fill: #212529;"  width=".6rem"><path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"/></svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" style="fill: #1c1c1c;" width=".6rem"><title>to be done</title><circle cx="50" cy="50" r="50"/></svg>
                 <span>run</span>
-              </a>
-            </small>
+              </button>
+            </a>
           </li>
         </ul>
       </div>
