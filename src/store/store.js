894ada07a868eeac42a3b2040f2a697c5009e762
vkasraj <vikasraj1911@gmail.com>
2018-02-10 10:42:43 +0530

    code settings: storing and fetching from sessionstorage completed

--- src/store/store.js
+++ src/store/store.js
@@ -4,55 +4,58 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state: {
-        htmlCode: '',
-        cssCode: '',
-        jsCode: '',
-        htmlConfig: {
-            htmlTagClasses: '',
-            headStuff: ''
-        },
-        cssConfig: {
-            stylesheets: ''
-        },
-        jsConfig: {
-            scripts: ''
-        },
-        output: '',
-        saved: true
+  state: {
+    htmlCode: "",
+    cssCode: "",
+    jsCode: "",
+    htmlConfig: {
+      htmlTagClasses: "",
+      headStuff: ""
     },
-    mutations: {
-        setHtmlCode(state, payload) {
-            state.saved = false;
-            state.htmlCode = payload;
-        },
-        setCssCode(state, payload) {
-            state.saved = false;
-            state.cssCode = payload;
-        },
-        setJsCode(state, payload) {
-            state.saved = false;
-            state.jsCode = payload;
-        },
-        setOutput(state, payload) {
-            state.output = payload;
-        },
-        setSaved(state, payload) {
-            state.saved = payload;
-        },
-        setHtmlConfig(state, payload) {
-            state.htmlConfig = payload;
-        },
-        setCssConfig(state, payload) {
-            state.cssConfig = payload;
-        },
-        setJsConfig(state, payload) {
-            state.jsConfig = payload;
-        }
+    cssConfig: {
+      stylesheets: ""
     },
-    actions: {
-        showOutput({ commit, state }) {
-            let output = `<head>
+    jsConfig: {
+      scripts: ""
+    },
+    output: "",
+    saved: true
+  },
+  mutations: {
+    setHtmlCode(state, payload) {
+      state.saved = false;
+      state.htmlCode = payload;
+    },
+    setCssCode(state, payload) {
+      state.saved = false;
+      state.cssCode = payload;
+    },
+    setJsCode(state, payload) {
+      state.saved = false;
+      state.jsCode = payload;
+    },
+    setOutput(state, payload) {
+      state.output = payload;
+    },
+    setSaved(state, payload) {
+      state.saved = payload;
+    },
+    setHtmlConfig(state, payload) {
+      sessionStorage.htmlConfig = JSON.stringify(payload);
+      state.htmlConfig = payload;
+    },
+    setCssConfig(state, payload) {
+      sessionStorage.cssConfig = JSON.stringify(payload);
+      state.cssConfig = payload;
+    },
+    setJsConfig(state, payload) {
+      sessionStorage.jsConfig = JSON.stringify(payload);
+      state.jsConfig = payload;
+    }
+  },
+  actions: {
+    showOutput({ commit, state }) {
+      let output = `<head>
                             <style>
                                 ${state.cssCode}
                             </style>
@@ -64,21 +67,27 @@ export const store = new Vuex.Store({
                             </script>
                         </body>`;
 
-            sessionStorage.html = state.htmlCode;
-            sessionStorage.css = state.cssCode;
-            sessionStorage.js = state.jsCode;
+      sessionStorage.html = state.htmlCode;
+      sessionStorage.css = state.cssCode;
+      sessionStorage.js = state.jsCode;
 
-            commit('setOutput', output);
-            commit('setSaved', true);
-        },
-        getCodeFromStorage({ commit }) {
-            let h = sessionStorage.getItem('html') || '';
-            let c = sessionStorage.getItem('css') || '';
-            let j = sessionStorage.getItem('js') || '';
+      commit("setOutput", output);
+      commit("setSaved", true);
+    },
+    getCodeFromStorage({ commit }) {
+      let _html = sessionStorage.getItem("html") || "";
+      let _css = sessionStorage.getItem("css") || "";
+      let _js = sessionStorage.getItem("js") || "";
+      let _htmlConfig = JSON.parse(sessionStorage.getItem("htmlConfig")) || "";
+      let _cssConfig = JSON.parse(sessionStorage.getItem("cssConfig")) || "";
+      let _jsConfig = JSON.parse(sessionStorage.getItem("jsConfig")) || "";
 
-            commit('setHtmlCode', h);
-            commit('setCssCode', c);
-            commit('setJsCode', j);
-        }
+      commit("setHtmlCode", _html);
+      commit("setCssCode", _css);
+      commit("setJsCode", _js);
+      commit("setHtmlConfig", _htmlConfig);
+      commit("setCssConfig", _cssConfig);
+      commit("setJsConfig", _jsConfig);
     }
-})
+  }
+});
